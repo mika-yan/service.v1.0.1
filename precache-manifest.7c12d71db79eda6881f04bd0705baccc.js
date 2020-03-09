88d2282c721d301b34a907342299a1777576aab7
unknown <kryuger.mikaelyan@inbox.ru>
2020-03-09 09:28:32 +0300

    Updates

---
+++ precache-manifest.7c12d71db79eda6881f04bd0705baccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0f4398bda083dbbae22d687542759f",
+    "url": "/teeeesst/index.html"
+  },
+  {
+    "revision": "7b65c86087bb0c26ef8b",
+    "url": "/teeeesst/static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "04492b2aa3e5bd2d9e8a",
+    "url": "/teeeesst/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7b65c86087bb0c26ef8b",
+    "url": "/teeeesst/static/js/2.bacf0dbc.chunk.js"
+  },
+  {
+    "revision": "04492b2aa3e5bd2d9e8a",
+    "url": "/teeeesst/static/js/main.c850897e.chunk.js"
+  },
+  {
+    "revision": "b512f67a6b1ea26855fd",
+    "url": "/teeeesst/static/js/runtime-main.bd3e523d.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/teeeesst/static/media/persik.4e1ec840.png"
+  }
+]);
