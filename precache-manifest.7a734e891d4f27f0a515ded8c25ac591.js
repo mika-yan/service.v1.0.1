738f2a2cf4968d604baf69ad559ce879de44d30b
mika-yan <kryuger.mikaelyan@inbox.ru>
2020-04-05 00:59:19 +0300

    Updates

---
+++ precache-manifest.7a734e891d4f27f0a515ded8c25ac591.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2a2bc0b110d5412279941cdd02b623",
+    "url": "/hayastan-/index.html"
+  },
+  {
+    "revision": "0a5162fb96b19b5deef5",
+    "url": "/hayastan-/static/css/2.3e9c613c.chunk.css"
+  },
+  {
+    "revision": "96fd4deafad2dfdec049",
+    "url": "/hayastan-/static/css/main.4bd95811.chunk.css"
+  },
+  {
+    "revision": "0a5162fb96b19b5deef5",
+    "url": "/hayastan-/static/js/2.fcc94ea0.chunk.js"
+  },
+  {
+    "revision": "96fd4deafad2dfdec049",
+    "url": "/hayastan-/static/js/main.7f759a60.chunk.js"
+  },
+  {
+    "revision": "e8bf836be6e1c0526284",
+    "url": "/hayastan-/static/js/runtime-main.d7244ff5.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/hayastan-/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/service.v1.0.1/precache-manifest.278a5629aaf50dd9396aa4ab9b258af9.js"
+  "/hayastan-/precache-manifest.7a734e891d4f27f0a515ded8c25ac591.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/service.v1.0.1/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hayastan-/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
